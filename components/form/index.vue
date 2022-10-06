fe434b4d1475b6f1003a4c35d0f302e8de7cb9b0
Kenneth Solomon <synecders@gmail.com>
2022-10-06 23:51:13 +0800

    dynamic field ang grid

--- components/form/index.vue
+++ components/form/index.vue
@@ -1,44 +1,42 @@
 <template>
   <ValidationObserver ref="observer" v-slot="{ valid, invalid }">
     <form @submit.prevent="onSubmit">
-      <div
-        v-for="field in fields"
-        :key="field.name"
-      >
-        <Input
-          :valid="valid"
-          :title="field.title"
-          :name="field.name"
-          :type="field.type"
-          :rules="field.rules"
-        />
-      </div>
-        <v-btn
-          type="submit"
-          :color="button.color"
-          :disabled="invalid"
+      <v-row>
+        <v-col
+          v-for="field in fields"
+          :key="field.name"
+          :cols="12 / field.cols"
         >
-          {{button.btn_name}} <v-icon class="ml-2">{{button.icon}}</v-icon>
-        </v-btn>
+          <Input
+            :valid="valid"
+            :title="field.title"
+            :name="field.name"
+            :type="field.type"
+            :rules="field.rules"
+          />
+        </v-col>
+      </v-row>
+      <v-btn type="submit" :color="button.color" :disabled="invalid">
+        {{ button.btn_name }} <v-icon class="ml-2">{{ button.icon }}</v-icon>
+      </v-btn>
     </form>
   </ValidationObserver>
 </template>
 
 <script>
 export default {
-  data: () => ({
-  }),
+  data: () => ({}),
   props: {
     button: Object, //color, btn_name, icon
     fields: Array, //rules, title, type
   },
   methods: {
     onSubmit(values) {
-      console.log(values.target.elements)
+      console.log(values.target.elements);
       // const { first_item } =  values.target.elements
       // console.log(first_item.', vaues)
-    }
-  }
+    },
+  },
 };
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,48 @@
 <template>
-  <Form
-    :fields="fields"
-    :button="button"
-  />
-
+  <Form :fields="fields" :button="button" />
 </template>
 
 <script>
-
-    // button: Object, //color, btn_name, icon
-    // fields: Array, //rules, name,title
+// button: Object, //color, btn_name, icon
+// fields: Array, //rules, name,title
 export default {
-  name: 'IndexPage',
+  name: "IndexPage",
   data: () => ({
-    fields:
-    [
+    fields: [
       {
-        name: 'first_item',
-        title: 'First Item',
-        rules: 'required',
-        type: 'text',
+        cols: 1,
+        name: "first_item",
+        title: "First Item",
+        rules: "required",
+        type: "text",
       },
       {
-        name: 'second_item',
-        title: 'Second Item',
-        rules: 'required',
-        type: 'text',
+        cols: 1,
+        name: "second_item",
+        title: "Second Item",
+        rules: "required",
+        type: "text",
       },
-
+      {
+        cols: 2,
+        name: "third_item",
+        title: "Third Item",
+        rules: "required",
+        type: "text",
+      },
+      {
+        cols: 2,
+        name: "fourth_item",
+        title: "Fourth Item",
+        rules: "required",
+        type: "text",
+      },
     ],
     button: {
       color: "success",
-      btn_name: 'Save',
-      icon: 'mdi-cash-check'
-    }
-  })
-}
+      btn_name: "Save",
+      icon: "mdi-cash-check",
+    },
+  }),
+};
 </script>
