7948511f5a943371871a609ee2ba2a1d8856bb74
Kenneth Solomon <synecders@gmail.com>
2022-10-11 23:27:21 +0800

    add tab_name on `property.vue`

--- pages/property.vue
+++ pages/property.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tab />
+  <Tab :tab_name="tab_name" />
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    tab_name: ["All Property", "Consumable", "Non-Consumable"],
+  }),
+};
 </script>
 
 <style scoped></style>
