726ed2c0c34eb48893219f331a3ecb469681f7c0
Kenneth Solomon <synecders@gmail.com>
2022-10-15 09:23:24 +0800

    add utility tools

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     "~/plugins/vee-validate.js",
-    "~/plugins/parse.js"
+    "~/plugins/parse.js",
+    "~/plugins/tools.js"
   ],
 
   // Global Middleware
---
+++ plugins/tools.js
@@ -0,0 +1,31 @@
+export default ({ app }, inject) => {
+  // Inject $hello(msg) in Vue, context and store.
+  inject('hello', (msg) => console.log(`Hello ${msg}!`))
+
+  // Sample Global Helper
+  inject('add', (num1, num2) => {
+    console.log(num1 + num2)
+  })
+
+
+  // Snackbars
+//   inject('unSetSnackbar', (store) => {
+//     store.commit('SET_SNACKBAR', {
+//           snackbar: {
+//             status: false,
+//             text: store.state.snackbar.text,
+//             timeout: 2000,
+//             color: 'error',
+//           },
+//     })
+//   })
+
+  inject('convertToCurrency', (money) => {
+	var formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'PHP',
+	});
+
+	return formatter.format(money);
+  })
+}
