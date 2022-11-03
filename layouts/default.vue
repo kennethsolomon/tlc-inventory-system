8824ba7608f28eceeced1f00f93aa87964064938
Kenneth Solomon <synecders@gmail.com>
2022-11-04 05:12:03 +0800

    remove unused data from `layouts/default.vue`

--- layouts/default.vue
+++ layouts/default.vue
@@ -181,24 +181,7 @@ export default {
       right: true,
       rightDrawer: false,
       title: "Inventory System",
-
-      totalDesserts: 0,
-      desserts: [],
       loading: true,
-      options: {},
-      headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
-      ],
     };
   },
   methods: {
