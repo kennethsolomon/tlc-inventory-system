f834a08eb21ae36bbb2c93bfbc25e7490eccb0e3
Kenneth Solomon <mr.kennethsolomon@gmail.com>
2022-11-28 21:52:17 +0800

    test responsive

--- pages/index.vue
+++ pages/index.vue
@@ -64,7 +64,7 @@
     />
     <div class="container">
       <v-row>
-        <v-col cols="6" lg="6" md="12" sm="12">
+        <v-col xl="6" lg="6" md="12" sm="12">
           <v-card color="primary" dark>
             <div class="d-flex flex-no-wrap justify-space-between">
               <div class="mb-5">
@@ -83,7 +83,7 @@
             </div>
           </v-card>
         </v-col>
-        <v-col cols="6" lg="6" md="12" sm="12">
+        <v-col xl="6" lg="6" md="12" sm="12">
           <v-card color="primary" dark>
             <div class="d-flex flex-no-wrap justify-space-between">
               <div class="mb-5">
