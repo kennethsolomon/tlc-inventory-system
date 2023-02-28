c72afbc27b72af3e15cbc316c17cd19f7c1f9430
kennethsolomon <mr.kennethsolomon@gmail.com>
2023-02-28 20:37:13 +0800

    change title to tlc

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s - TLC Inventory System",
-    title: "dict_inventory_system",
+    title: "tlc_inventory_system",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
