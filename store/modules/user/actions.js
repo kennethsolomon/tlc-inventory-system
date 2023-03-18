0a06c2a446ea34a94030d2ea64027a476b327cdb
kennethsolomon <mr.kennethsolomon@gmail.com>
2023-03-19 06:01:35 +0800

    adjust sore

--- store/modules/user/actions.js
+++ store/modules/user/actions.js
@@ -1,30 +1,28 @@
-export default{
-	// async getUser ({ commit }) {
-	// 	const employees = await axios.get(`http://localhost:8000/api/employees`)
-	// 	commit('SET_EMPLOYEES', employees.data)
-	// }
-	async getLogs ({ commit }) {
-		const logs = await this.$axios.$get(`logs`)
-		commit('SET_LOGS', logs)
-		return logs
-	},
+export default {
+  // async getUser ({ commit }) {
+  // 	const employees = await axios.get(`http://localhost:8000/api/employees`)
+  // 	commit('SET_EMPLOYEES', employees.data)
+  // }
+  async getLogs({ commit }) {
+    const logs = await this.$axios.$get(`logs`);
+    commit("SET_LOGS", logs);
+    return logs;
+  },
 
-	async getIdToken ({ commit }) {
-		const logs = await this.$axios.$get(`logs`)
-		commit('SET_LOGS', logs)
-		return logs
-	},
+  async getIdToken({ commit }) {
+    const logs = await this.$axios.$get(`logs`);
+    commit("SET_LOGS", logs);
+    return logs;
+  },
 
+  async getMaintenances({ commit }) {
+    const maintenance = await this.$axios.$get(`maintenance`);
+    commit("SET_MAINTENANCE", maintenance);
+    return maintenance;
+  },
 
-	async getMaintenances ({ commit }) {
-		const maintenance = await this.$axios.$get(`maintenance`)
-		commit('SET_MAINTENANCE', maintenance)
-		return maintenance
-	},
-
-	async updateUser ({}, form) {
-		const user = await this.$axios.$post(`update_account`, form)
-		return user
-	},
-
-}
+  async updateUser({}, form) {
+    const user = await this.$axios.$post(`update-or-create-user`, form);
+    return user;
+  },
+};
